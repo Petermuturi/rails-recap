2e54a68b9292c79c4b608e710a417d611ab96b28
Peter Muturi <pmuturi65@gmail.com>
2015-08-25 12:01:50 +0300

    changed

--- jobsboard/app/controllers/jobs_controller.rb
+++ jobsboard/app/controllers/jobs_controller.rb
@@ -1,14 +1,14 @@
 class JobsController < ApplicationController
   def index
-  	@jobs = Job.page(params[:page]).per(25)
+  	 @jobs = Job.all
   end
 
   def new
-  	@job = Job.new(params.require(:job).permit(:title, :company, :url))
+   @job = Job.new
   end
 
   def create
-  @job = Job.new
+   	@job = Job.new(params.require(:job).permit(:title, :company, :url))
   if @job.save
     redirect_to root_path
   else
